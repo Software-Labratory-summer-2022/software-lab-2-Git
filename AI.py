6327fc675c6cab4c401308fc69ceefa66f1b49cf
Danial Erfanian <erfaniiyanerfan@gmail.com>
2022-07-29 13:03:34 +0430

    FEAT: Translate simple emojis

--- AI.py
+++ AI.py
@@ -1,2 +1,6 @@
+import text_converter
+
+
 def get_response(message):
+    message = text_converter.replace_emoji(message)
     yield message
---
+++ text_converter.py
@@ -0,0 +1,19 @@
+import emoji
+
+
+def dictinary_replacing(string, replace):
+    for key, value in replace.items():
+        string = string.replace(key, value)
+    return string
+
+
+def replace_emoji(message):
+    char_emojis = {
+        ':)': ':smiling_face_with_smiling_eyes:',
+        ':(': ':disappointed_face:',
+        ';)': ':winking_face:',
+        ':D': ':grinning_face_with_big_eyes:',
+    }
+    # This replace replacing pattern that defined in char_emojis
+    message = dictinary_replacing(message, char_emojis)
+    return emoji.emojize(message)
