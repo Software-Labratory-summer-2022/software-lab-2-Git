ab7b19e52dd4f44d664021947011a79cefa52f02
a.h.hadian <a.h.hadian@gmail.com>
2022-07-29 13:03:26 +0430

    Feat: add defined scripts

--- AI.py
+++ AI.py
@@ -1,2 +1,16 @@
+DEFINED_SCRIPTES = {
+    "سلام": "علیک سلام",
+    "چطوری؟": "خوبم تو چطوری؟"}
+
+
+def is_script(message):
+    return message.startswith("+ ")
+
+
 def get_response(message):
-    yield message
+    if is_script(message):
+        script = message[2:]
+        if script in DEFINED_SCRIPTES:
+            yield DEFINED_SCRIPTES[script]
+    else:
+        yield message
